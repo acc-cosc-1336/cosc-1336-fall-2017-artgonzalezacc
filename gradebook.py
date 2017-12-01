05a4da79814238c3e2995a7a5491f0727410cacb
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2017-11-30 20:50:52 -0600

    Homework 11-add inheritance and transcript

--- gradebook.py
+++ gradebook.py
@@ -1,6 +1,8 @@
 from course import Course
 from student import Student
 from enrollment import Enrollment
+from professor import Professor
+from transcript import Transcript
 
 class Gradebook:
 
@@ -23,23 +25,41 @@ class Gradebook:
         s = Student(7, "Nino", "Olivetto", "09012005")
         self.students[s.student_id] = s
 
+        self.professors = {}
+
+        #professor_id   first_name   last_name  hire_date
+        p = Professor(1, "Kim", "Abercrombie", "1995-03-11") 
+        self.professors[p.professor_id] = p
+
+        p = Professor(2, "Fadi", "Fakhouri", "2002-07-06") 
+        self.professors[p.professor_id] = p
+
+        p = Professor(3, "Roger", "Harui", "1998-07-01") 
+        self.professors[p.professor_id] = p
+
+        p = Professor(4, "Candace", "Kapoor", "2001-01-15")
+        self.professors[p.professor_id] = p
+
+        p = Professor(5, "Roger", "Zheng", "2004-02-12") 
+        self.professors[p.professor_id] = p
+
 
         self.courses = {}
 
         #add to course dictionary
-        c = Course(1050, "Chemistry", 3)
+        c = Course(1050, "Chemistry", 3, self.professors[1])
         self.courses[c.course_id] = c
-        c = Course(4022, "Microeconomics", 3)
+        c = Course(4022, "Microeconomics", 3, self.professors[2])
         self.courses[c.course_id] = c
-        c = Course(4041, "Macroeconomics", 3)
+        c = Course(4041, "Macroeconomics", 3, self.professors[3])
         self.courses[c.course_id] = c
-        c = Course(1045, "Calculus", 4)
+        c = Course(1045, "Calculus", 4, self.professors[4])
         self.courses[c.course_id] = c
-        c = Course(3141, "Trigonometry", 4)
+        c = Course(3141, "Trigonometry", 4, self.professors[5])
         self.courses[c.course_id] = c
-        c = Course(2021, "Composition", 3)
+        c = Course(2021, "Composition", 3, self.professors[1])
         self.courses[c.course_id] = c
-        c = Course(2042, "Literature", 4)
+        c = Course(2042, "Literature", 4, self.professors[2])
         self.courses[c.course_id] = c
 
 
@@ -96,26 +116,54 @@ class Gradebook:
 
     def main(self):
 
-        keep_going = 'y'
+        choice = ''
 
-        while keep_going == 'y':
+        while choice != 'e':
 
-            enroll_key = int(input("Enter enroll key"))
+            choice = self.__main_menu()
 
-            if enroll_key in self.enrollments:
-                enroll = self.enrollments.get(enroll_key)
+            if choice == '1':
 
-                grade = input("Enter grade: ")
-                enroll.grade = grade
+                enroll_key = int(input("Enter enroll key"))
 
-            else:
-                print("Key doesn't exist")
+                if enroll_key in self.enrollments:
+                    enroll = self.enrollments.get(enroll_key)
 
-            keep_going = input("Enter y to continue...")
+                    grade = input("Enter grade: ")
+                    enroll.grade = grade
 
-        for enrollment in self.enrollments.values():
-            enrollment.print_record()
+                else:
+                    print("Key doesn't exist")
 
+            elif choice == '2':
+
+                student_id =  int(input("Enter student id: "))
+
+                if student_id in self.students:
+
+                    student = self.students.get(student_id)
+                    transcript = Transcript(self.enrollments)
+                    transcript.print_transcript(student)
+
+                else:
+                    print("Student does not exist")
+
+            elif choice == '3':
+
+                for enrollment in self.enrollments.values():
+                    enrollment.print_record()
+
+    def __main_menu(self):
+
+        print()
+        print("Academic Menu")
+        print("1) Update Grade")
+        print("2) Print Student GPA")
+        print("3) Print All Enrollments")
+        print()
+
+        return input("Enter 1,2, or 3.  e to exit")
+        
 
 gradebook = Gradebook()
 gradebook.main()
